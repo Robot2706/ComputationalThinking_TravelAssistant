d28831e84b6b20dd977f2b95a9115c4659b309a7
cpm-201 <chau.pmy@gmail.com>
2025-11-30 21:04:34 +0700

    fix hiển thị chatbot-result

--- frontend/js/chatbot-result.js
+++ frontend/js/chatbot-result.js
@@ -36,3 +36,19 @@ document.querySelectorAll(".hotel-card").forEach((card, index) => {
         window.location.href = "../chatBot_detail/chatBot_detail.html";
     });
 });
+
+const askInput = document.getElementById("askInput");
+const aiMessage = document.getElementById("aiMessage");
+
+askInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter" && askInput.value.trim() !== "") {
+        
+        const userText = askInput.value.trim();
+        console.log("User asked:", userText);
+
+        // Tạm thời cho AI trả lời cứng
+        aiMessage.textContent = `Mình đã nhận câu trả lời: "${userText}". Bạn muốn mình hỗ trợ điều gì thêm?`;
+
+        askInput.value = "";
+    }
+});
